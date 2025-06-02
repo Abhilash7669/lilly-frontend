46d9989ad403eff777d018d00bbbb3b71d718632
Abhilash7669 <abhilashsk1998@gmail.com>
2025-06-02 15:02:43 +0530

    testing

--- src/app/(protected)/work-space/home/page.tsx
+++ src/app/(protected)/work-space/home/page.tsx
@@ -1,10 +1,17 @@
 "use client";
 
+import { useEffect } from "react";
+
 type Props = object;
 
 export default function Page({}: Props) {
 
-  
+  useEffect(() => {
+
+
+
+  }, []);
+
 
   return (
     <div>page</div>
--- src/components/layout/authenticated-layout.tsx
+++ src/components/layout/authenticated-layout.tsx
@@ -7,6 +7,7 @@ type Props = {
   children: React.ReactNode;
 };
 
+
 export default function AuthenticatedLayout({ children }: Props) {
   return (
     <SidebarProvider>
