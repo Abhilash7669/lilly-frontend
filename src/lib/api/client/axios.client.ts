186ccfadf031107bbf2375b5ee00570497ff445b
Abhilash7669 <abhilashsk1998@gmail.com>
2025-07-14 11:27:13 +0530

    feat: added put method in axios_client.ts

--- src/lib/api/client/axios.client.ts
+++ src/lib/api/client/axios.client.ts
@@ -136,7 +136,77 @@ export const AXIOS_CLIENT = {
       return null;
     }
   },
-  delete: async function<T>(url: string, config?: AxiosRequestConfig): Promise<T | null> {
+  put: async function <T, U>(
+    url: string,
+    data: T,
+    config?: AxiosRequestConfig
+  ): Promise<U | null> {
+    const token = await getCookie();
+
+    let m_urlParams;
+
+    if (config?.params) m_urlParams = qs.stringify(config.params);
+
+    try {
+      const m_data = await axios.put<U & CustomResponse>(
+        `${ENV.BASEURL}${url}`,
+        data,
+        {
+          headers: {
+            Authorization: `Bearer ${token ? token : null}`,
+          },
+          params: m_urlParams || null,
+          signal: AbortSignal.timeout(5000),
+          timeoutErrorMessage: "Request Timed out",
+          ...config,
+        }
+      );
+
+      const isSuccess = m_data.data.success;
+
+      toast(m_data.data.title || "Success", {
+        description: m_data.data.message || "Welcome",
+        duration: 3400,
+        style: {
+          background: isSuccess
+            ? "var(--custom-success)"
+            : "var(--custom-error)",
+          border: isSuccess
+            ? "1px var(--custom-success-border) solid"
+            : "1px var(--custom-error-border) solid",
+          color: "var(--custom-toast-text)",
+          backdropFilter: "blur(120px)",
+          WebkitBackdropFilter: "blur(120px)",
+        },
+      });
+
+      return m_data.data;
+    } catch (error) {
+      const axiosError = error as AxiosError<CustomResponse>;
+
+      if (!axiosError.response) {
+        console.error(error);
+        throw axiosError;
+      }
+      toast(axiosError.response.data.title || "Error", {
+        description: axiosError.response.data.message || "Something went wrong",
+        duration: 3400,
+        style: {
+          background: "var(--custom-error)",
+          border: "1px var(--custom-error-border) solid",
+          color: "var(--custom-toast-text)",
+          backdropFilter: "blur(120px)",
+          WebkitBackdropFilter: "blur(120px)",
+        },
+      });
+
+      return null;
+    }
+  },
+  delete: async function <T>(
+    url: string,
+    config?: AxiosRequestConfig
+  ): Promise<T | null> {
     const token = await getCookie();
 
     let m_urlParams;
@@ -151,7 +221,7 @@ export const AXIOS_CLIENT = {
         params: m_urlParams || null,
         signal: AbortSignal.timeout(5000),
         timeoutErrorMessage: "Request Timed out",
-        ...config
+        ...config,
       });
 
       toast(m_data.data.title || "Success", {
