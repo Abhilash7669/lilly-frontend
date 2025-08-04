3aa90daab222dccba9bddd3ee77c7653c4e54bc6
Abhilash7669 <abhilashsk1998@gmail.com>
2025-08-04 15:30:12 +0530

    corrected object property for avatar

--- src/components/side-bar/nav-user.tsx
+++ src/components/side-bar/nav-user.tsx
@@ -71,7 +71,7 @@ export function NavUser() {
                 className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
               >
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={userAvatar || "/avatars/goku-blue.jpg"} alt="avatar" />
+                  <AvatarImage src={userAvatar || "/avatars/goku-blue.jpg"} className="object-cover" alt="avatar" />
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
@@ -89,7 +89,7 @@ export function NavUser() {
               <DropdownMenuLabel className="p-0 font-normal">
                 <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                   <Avatar className="h-8 w-8 rounded-lg">
-                    <AvatarImage src={userAvatar || "/avatars/goku-blue.jpg"} alt={userName || "avatar"} />
+                    <AvatarImage src={userAvatar || "/avatars/goku-blue.jpg"} className="object-cover" alt={userName || "avatar"} />
                     <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                   </Avatar>
                   <div className="grid flex-1 text-left text-sm leading-tight">
