2ab4edbba71773ea250c82887e7eb6256b3fe966
Abhilash7669 <abhilashsk1998@gmail.com>
2025-08-09 21:16:35 +0530

    added temporary text-loader on redirecting from oauth

--- src/app/(auth)/redirect-oauth/page.tsx
+++ src/app/(auth)/redirect-oauth/page.tsx
@@ -28,7 +28,7 @@ export default function Page() {
         setIsError(true);
         return;
       }
-      setUserAvatar(avatar);    
+      setUserAvatar(avatar);
       setUsername(userName);
 
       const isCookieSet = await setCookieValue({
@@ -52,4 +52,65 @@ export default function Page() {
         Unauthorised ERROR!
       </h1>
     );
+
+  // const [test, setTest] = useState({ dia: 200, rotate: 360 });
+  // const [items, _] = useState(10);
+  // const dummyData = Array.from({ length: items }, (_, i) => i);
+
+  // useEffect(() => {
+  //   const timeout = setTimeout(() => {
+  //     setTest((prev) => ({
+  //       dia: prev.dia === 80 ? 200 : 80,
+  //       rotate: prev.rotate === 360 ? 10 : 360,
+  //     }));
+  //   }, 2600);
+
+  //   return () => clearTimeout(timeout);
+  // }, [test.dia]);
+
+  // const radius = (test.dia / 10) * 10; // scale logic
+  // const n = dummyData.length;
+
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      <h1 className="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 animate-pulse text-xl">
+        Redirecting you
+      </h1>
+      {/* <div
+        className="relative rounded-full"
+        style={{
+          height: `${test.dia}px`,
+          width: `${test.dia}px`,
+          transformOrigin: "center",
+          transform: `rotate(${test.rotate}deg)`,
+          transition: "all 2s ease",
+        }}
+      > */}
+        {/* dots */}
+        {/* {dummyData.map((_, index) => {
+          const sectorAngle = (360 / n) * index;
+          const angleInRad = (sectorAngle * Math.PI) / 180;
+
+          const x = Math.cos(angleInRad) * radius - 5;
+          const y = Math.sin(angleInRad) * radius - 5;
+
+          const color = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(
+            Math.random() * 255
+          )}, ${Math.floor(Math.random() * 255)})`;
+
+          return (
+            <div
+              key={index}
+              className="absolute top-2/4 left-2/4 h-[10px] w-[10px] rounded-full"
+              style={{
+                backgroundColor: color,
+                transform: `translate(${x}px, ${y}px)`,
+                transition: "all 3s ease",
+              }}
+            />
+          );
+        })} */}
+      {/* </div> */}
+    </div>
+  );
 }
